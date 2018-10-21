fe13164303d1674ad120dd1fb91a9b5b133105e3
Ilia Syrtsou <ilia_syrtsou@epam.com>
2018-10-22 02:22:52 +0300

    add test for HomeController

--- Northwind.Web.Tests/Controllers/HomeControllerTests.cs
+++ Northwind.Web.Tests/Controllers/HomeControllerTests.cs
@@ -1,7 +1,23 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Web.Controllers;
+using Xunit;
+
 namespace Northwind.Web.Tests.Controllers
 {
     public class HomeControllerTests
     {
-        
+        [Fact]
+        public void Index_GetAllProducts_ShouldReturn10Products()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            result.Should().BeOfType<ViewResult>();
+        }
     }
 }
