5ab79ad141309da2ed6c71ae1ecd511b115a4683
Ilia Syrtsou <ilia.syrtsou@clarivate.com>
2018-10-24 16:59:54 +0300

    delete oldest cached images and preserve maximum number of files that is configured in appsettings

--- Northwind.Web/BackgroundTasks/TrackMaxCachedImagesTask.cs
+++ Northwind.Web/BackgroundTasks/TrackMaxCachedImagesTask.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -7,14 +10,14 @@ using Microsoft.Extensions.Logging;
 
 namespace Northwind.Web.BackgroundTasks
 {
-    internal class TrackMaxCachedImagesTask : IHostedService, IDisposable
+    internal class TrackMaxCachedImagesService : IHostedService, IDisposable
 {
     private readonly ILogger _logger;
     private readonly IConfiguration _configuration;
     private Timer _timer;
 
-    public TrackMaxCachedImagesTask(
-        ILogger<TrackMaxCachedImagesTask> logger,
+    public TrackMaxCachedImagesService(
+        ILogger<TrackMaxCachedImagesService> logger,
         IConfiguration configuration)
     {
         _configuration = configuration;
@@ -23,22 +26,47 @@ namespace Northwind.Web.BackgroundTasks
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Timed Background Service is starting.");
+        _logger.LogInformation("TrackMaxCachedImagesService is initializing...");
 
         _timer = new Timer(DoWork, null, TimeSpan.Zero, 
-            TimeSpan.FromMinutes(1));
+            TimeSpan.FromSeconds(30));
+
+        _logger.LogInformation("...Done.");
 
         return Task.CompletedTask;
     }
 
     private void DoWork(object state)
     {
-        _logger.LogInformation("Timed Background Service is working.");
+        int maxFilesNumber;
+
+        _logger.LogInformation("Max cached files check started.");
+        try {
+            maxFilesNumber = Convert.ToInt32(_configuration["Cache:Image:FileStorage:MaxFilesNumber"]);
+        } 
+        catch {
+            maxFilesNumber = 2;
+        }
+        
+        var directory = _configuration["Cache:Image:FileStorage:Directory"];
+
+        if(Directory.Exists(directory)) {
+            var filesToDelete = new DirectoryInfo(directory)
+                    .GetFiles()
+                    .OrderByDescending(x => x.LastWriteTime)
+                    .Skip(maxFilesNumber);
+            
+            foreach (var fi in filesToDelete) 
+            {
+                fi.Delete();
+            }
+        }
+        _logger.LogInformation("Max cached files check finished.");
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Timed Background Service is stopping.");
+        _logger.LogInformation("TrackMaxCachedImagesService is stopping...");
 
         _timer?.Change(Timeout.Infinite, 0);
 
