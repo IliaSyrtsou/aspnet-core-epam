3e5fef962798e3c2e63498c332fa2e91354825e2
Ilia Syrtsou <ilia_syrtsou@epam.com>
2018-10-24 10:37:16 +0300

    add background task for cache cleaning

---
+++ Northwind.Web/BackgroundTasks/TrackMaxCachedImagesTask.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Northwind.Web.BackgroundTasks
+{
+    internal class TrackMaxCachedImagesTask : IHostedService, IDisposable
+{
+    private readonly ILogger _logger;
+    private readonly IConfiguration _configuration;
+    private Timer _timer;
+
+    public TrackMaxCachedImagesTask(
+        ILogger<TrackMaxCachedImagesTask> logger,
+        IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Timed Background Service is starting.");
+
+        _timer = new Timer(DoWork, null, TimeSpan.Zero, 
+            TimeSpan.FromMinutes(1));
+
+        return Task.CompletedTask;
+    }
+
+    private void DoWork(object state)
+    {
+        _logger.LogInformation("Timed Background Service is working.");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Timed Background Service is stopping.");
+
+        _timer?.Change(Timeout.Infinite, 0);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+}
+}
+
